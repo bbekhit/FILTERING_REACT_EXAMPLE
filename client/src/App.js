6ed3972328b4abbd2076b72d906fe5ecfd94e9f0
Boutros Bekhit <bbekhit@apple.com>
2019-12-16 15:29:14 -0800

    test commit

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,7 @@ function App() {
 	console.log('hello')
 	return (
 		<div className='App'>
-			<header className='App-header'>
+			         <header className='App-header'>
 				<img src={logo} className='App-logo' alt='logo' />
 				<p>
 					Edit <code>src/App.js</code> and save to reload.
